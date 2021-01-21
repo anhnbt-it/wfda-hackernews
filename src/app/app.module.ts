e435422e9fdeaf84e404411e217d448da26c726b
anhnbt-it <anhnbt.it@gmail.com>
2021-01-21 09:21:46 +0700

    Add Article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { ArticleService } from '../service/article.service';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  articles = this.articleService.findAll();
+  articleForm = this.formBuilder.group({
+    title: '',
+    url: ''
+  });
+
+  constructor(
+    private articleService: ArticleService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.articleService.save(this.articleForm.value);
+    console.warn('Your article has been submitted', this.articleForm.value);
+    this.articleForm.reset();
+  }
+
+}
